972dbdda15c2b78d6b33556230926e148a22452e
Suriya R <kartsuri89@gmail.com>
2021-09-03 00:28:53 +0530

    MovieInfo Microservices - First commit

---
+++ movie-info-service/src/main/java/io/suriya/movie/info/model/MovieSummary.java
@@ -0,0 +1,35 @@
+package io.suriya.movie.info.model;
+
+public class MovieSummary {
+	
+	private String adult;
+	private String imdb_id;
+	private String title;
+	private String overview;
+	
+	public String getAdult() {
+		return adult;
+	}
+	public void setAdult(String adult) {
+		this.adult = adult;
+	}
+	public String getImdb_id() {
+		return imdb_id;
+	}
+	public void setImdb_id(String imdb_id) {
+		this.imdb_id = imdb_id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getOverview() {
+		return overview;
+	}
+	public void setOverview(String overview) {
+		this.overview = overview;
+	}
+
+}
---
+++ movie-info-service/src/test/java/io/suriya/movie/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.suriya.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
